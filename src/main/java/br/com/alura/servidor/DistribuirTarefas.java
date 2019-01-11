22301b55cd2d4cb73d55b7cd367b73a6af4d37c3
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-01-10 23:58:30 -0200

    Aula 6 - Future com Callable

--- src/main/java/br/com/alura/servidor/DistribuirTarefas.java
+++ src/main/java/br/com/alura/servidor/DistribuirTarefas.java
@@ -3,7 +3,9 @@ package br.com.alura.servidor;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class DistribuirTarefas implements Runnable {
 
@@ -48,8 +50,27 @@ public class DistribuirTarefas implements Runnable {
 				}
 				case "c2": {
 					saidaCliente.println("Confirmação do comando c2");
-					ComandoC2 c2 = new ComandoC2(saidaCliente);
-					this.threadPool.execute(c2);
+
+					Callable c2WS = new ComandoC2ChamaWS(saidaCliente);
+					// passando os comandos para o pool, resultado é um Future
+					Future<String> futureWS = this.threadPool.submit(c2WS);
+
+					Callable c2Banco = new ComandoC2AcessaBanco(saidaCliente);
+					Future<String> futureBanco = this.threadPool.submit(c2Banco);
+
+					// classes Callable devem ser chamadas pelo ExecutorService com submit
+
+					/**
+					 * String resultadoWS = futureWS.get();
+					 * 
+					 * Ao chamar o método get() do Future, vamos bloquear a thread (a thread fica
+					 * travada) e não queremos isso, pois o switch executa a thread para receber
+					 * qualquer comando, e não queremos travar o recebimento de outros comandos.
+					 */
+
+					// passando a tarefa para juntar os resultados para o pool
+					this.threadPool.submit(new JuntaResultadosFutureWSFutureBanco(futureWS, futureBanco, saidaCliente));
+
 					break;
 				}
 
